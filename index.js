2f04b562edbc99db258d278f7435dfd83755f735
Phil Gates-Shannon <phil.gates-shannon@wmcglobal.com>
2017-12-28 09:47:55 -0500

    Add ugly implementation of "start oauth2" function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+import crypto from 'crypto';
 import _ from 'lodash';
+import superagent from 'superagent';
+import url from 'url';
 
 let factoryOptions = null;
 
@@ -33,7 +36,15 @@ function initialize( options ) {
     if ( !_.isNull( factoryOptions ) ) {
         throw new Error( messagesFactory.factoryAlreadyInitialized() );
     }
-    factoryOptions = options;
+
+    const derivedOptions = {
+        authorizationUrl: 'https://' + options.domain + '/authorize',
+        tokenUrl:         'https://' + options.domain + '/oauth/token',
+        userInfoUrl:      'https://' + options.domain + '/userinfo',
+        apiUrl:           'https://' + options.domain + '/api'
+    };
+    factoryOptions = _.merge( {}, options, derivedOptions );
+    // console.log( `45: ${JSON.stringify( factoryOptions, null, 4 )}` );
 }
 
 export const messagesFactory = {
@@ -63,7 +74,11 @@ export default GladosFactory;
 
 // --- GLADOS OBJECT FUNCTIONS ---
 
-function completeOAuth2() {}
+function completeOAuth2() {
+    const callbackUrl = 'https://calypso.sword:5426/login/auth-complete?code=jaWcnFTvfS00SfSA&state=mYLMVTE2mDvtBV9WpEnsyyET%2F%2B6qNY2YoZ2ROAD05WY%3D';
+    const callbackUrlParts = url.parse( callbackUrl );
+    console.log( JSON.stringify( callbackUrlParts, null, 4 ) );
+}
 
 function ensureAuthenticated() {}
 
@@ -71,4 +86,58 @@ function getLoginHandler() {}
 
 function logout() {}
 
-function startOAuth2() {}
+function startOAuth2() {
+    // const params = {
+    //     clientId: factoryOptions.clientId,
+    //     domain: factoryOptions.domain,
+    //     redirectUri: factoryOptions.callbackUrl,
+    //     audience: `https://${factoryOptions.domain}/userinfo`,
+    //     responseType: 'code',
+    //     scope: 'openid email'
+    // };
+
+    // TODO Run this asynchronously
+    const csrfToken = crypto.randomBytes(32).toString('base64');
+
+    const oauthParams = {
+        audience: `https://${factoryOptions.domain}/userinfo`,
+        client_id: factoryOptions.clientId,
+        // connection: '',
+        // prompt: '',
+        // redirect_uri: factoryOptions.callbackUrl,
+        redirect_uri: 'https://calypso.sword:5426/login/auth-complete',
+        response_type: 'code',
+        scope: 'openid email',
+        state: csrfToken
+    };
+
+    const actualQueryParams = {
+        url: 'https://ickyzoo.auth0.com/login',
+        client: 'JoVzWhQOwQwIkialwg6uY5GfOAhfdI_A',
+        protocol: 'oauth2',
+        redirect_uri: 'https://calypso.sword:5426/login/auth-complete',
+        audience: 'https://ickyzoo.auth0.com/userinfo',
+        response_type: 'code',
+        scope: 'openid email',
+        state: 'ICYdxeXGLiWmUE_8CHl3WbELnfJQt3Zz'
+    };
+
+    // console.log( `102: ${JSON.stringify( factoryOptions, null, 4 )}` );
+    let authorizationUrlParts = url.parse( factoryOptions.authorizationUrl, true );
+    authorizationUrlParts.query = _.merge( {}, authorizationUrlParts.query, oauthParams );
+    let authorizationUrl = url.format(authorizationUrlParts);
+    console.log( JSON.stringify( authorizationUrlParts, null, 4 ) );
+    console.log( authorizationUrl );
+
+    // superagent
+    //     .get( authorizationUrl )
+    //     .then( response => {
+    //         console.log( JSON.stringify( response, null, 4 ) );
+    //     } )
+    //     .catch( error => console.error( error ) );
+
+    // Return an Express middleware function
+    return function( request, response ) {
+        response.redirect( authorizationUrl );
+    };
+}
