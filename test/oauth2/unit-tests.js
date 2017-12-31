ea68be723c49e2525bfc49f859c8f49773ff685b
Phil Gates-Shannon <phil.gates-shannon@wmcglobal.com>
2017-12-31 15:41:31 -0500

    Remove guts of most functions without unit tests

--- test/oauth2/unit-tests.js
+++ test/oauth2/unit-tests.js
@@ -18,7 +18,7 @@ const utils = {
 chai.use( sinonChai );
 chai.use( dirtyChai );
 
-describe( 'Glados includes an OAuth2 module that', function() {
+describe.only( 'Glados includes an OAuth2 module that', function() {
     context( 'has a `configure` method. This method', function() {
         let app = null;
         let options = null;
@@ -214,7 +214,7 @@ describe( 'Glados includes an OAuth2 module that', function() {
         } );
     } );
 
-    context.only( 'has a `completeOAuth2` function that returns a middleware function, which', function() {
+    context( 'has a `completeOAuth2` function that returns a middleware function, which', function() {
         let anonIdStub = null;          // stub session.setAnonymousSession
         let expressApp = {
             locals: { }
