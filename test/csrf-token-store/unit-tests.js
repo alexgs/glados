67072ca7353ee5ed1e2848ab7ecc404f0df78fdd
Phil Gates-Shannon <phil.gates-shannon@wmcglobal.com>
2017-12-31 20:11:10 -0500

    Implement many tests for CSRF Token Store module

--- test/csrf-token-store/unit-tests.js
+++ test/csrf-token-store/unit-tests.js
@@ -4,18 +4,20 @@ import dirtyChai from 'dirty-chai';
 import _ from 'lodash';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-import url from 'url';
-import defaultCsrfStore from '../../lib/csrf-token-store';
 
-import session from '../../lib/session';
+import csrfStore, { messageFactory, _reset as resetStore } from '../../lib/csrf-token-store';
 
 chai.use( sinonChai );
 chai.use( chaiAsPromised );
 chai.use( dirtyChai );
 
-// TODO >>> Add tests for `verifyToken` and other functions with failure modes <<<
+describe.only( 'Glados includes a CRSF Token Store module that', function() {
+    const badTokens = [ 9, [ 'one', 'two' ], { foo: 'bar' }, false ];
 
-describe( 'Glados includes a CRSF Token Store module that', function() {
+    beforeEach( function() {
+        resetStore();
+    } );
+
     context( '(working with "Chai as Promised")', function() {
         it( 'passes one canary test', function() {
             return expect( Promise.resolve( 2 + 2 ) ).to.eventually.equal( 4 );
@@ -31,18 +33,65 @@ describe( 'Glados includes a CRSF Token Store module that', function() {
     } );
 
     context( 'has a function `storeToken`, which', function() {
-        // TODO >>> Add tests <<<
+        it( 'throws an error if the `token` argument is not a string', function() {
+            badTokens.forEach( function( badToken ) {
+                expect( function() {
+                    csrfStore.storeToken( badToken );
+                } ).to.throw( Error, messageFactory.tokenMustBeString( badToken ) )
+            } );
+        } );
+
+        it( 'throws an error if the `token` is already in the store', function() {
+            const twofer = 'may-the-schwartz-be-with-you';
+            expect( function() {
+                csrfStore.storeToken( twofer )
+            } ).to.not.throw();
+            expect( function() {
+                csrfStore.storeToken( twofer )
+            } ).to.throw( Error, messageFactory.storeHasToken( twofer ) );
+        } );
     } );
 
     context( 'has a function `verifyToken`, which', function() {
-        it( 'throws an error if the `token` argument is not a string' );
+        const ludicrousSpeed = 'theyve-gone-to-plaid';
 
-        context( '(if the token is in the store)', function() {
-            it( 'returns true' );
-
-            it( 'removes the token from the store' );
+        it( 'throws an error if the `token` argument is not a string', function() {
+            badTokens.forEach( function( badToken ) {
+                expect( function() {
+                    csrfStore.verifyToken( badToken );
+                } ).to.throw( Error, messageFactory.tokenMustBeString( badToken ) )
+            } );
         } );
 
-        it( '(if the token is not the store) returns false', function() {} );
+        context( '(if the token is in the store)', function() {
+            it( 'returns true', function() {
+                csrfStore.storeToken( ludicrousSpeed );
+                expect( csrfStore.verifyToken( ludicrousSpeed ) ).to.equal( true );
+            } );
+
+            it( 'removes the token from the store', function() {
+                // Store a token
+                csrfStore.storeToken( ludicrousSpeed );
+
+                // Try to store the same token and catch an error
+                try {
+                    csrfStore.storeToken( ludicrousSpeed );
+                } catch ( error ) {
+                    expect( error.message ).to.equal( messageFactory.storeHasToken( ludicrousSpeed ) );
+                }
+
+                // Verify the token
+                expect( csrfStore.verifyToken( ludicrousSpeed ) ).to.equal( true );
+
+                // Store the token without an error
+                expect( function() {
+                    csrfStore.storeToken( ludicrousSpeed );
+                } ).to.not.throw();
+            } );
+        } );
+
+        it( '(if the token is not the store) returns false', function() {
+            expect( csrfStore.verifyToken( ludicrousSpeed ) ).to.equal( false );
+        } );
     } );
 } );
