a72f31c66a7647d8f0b1ad7d57ffb31556ed918e
Phil Gates-Shannon <phil.gates-shannon@wmcglobal.com>
2017-12-31 19:28:29 -0500

    Add test stubs for CSRF Token Store module

--- test/csrf-token-store/unit-tests.js
+++ test/csrf-token-store/unit-tests.js
@@ -16,10 +16,33 @@ chai.use( dirtyChai );
 // TODO >>> Add tests for `verifyToken` and other functions with failure modes <<<
 
 describe( 'Glados includes a CRSF Token Store module that', function() {
-    it( 'passes a canary test', function( done ) {
-        // return expect( Promise.resolve( { foo: 'bar' } ) ).to.eventually.have.property( 'foo' );
-        // return expect( Promise.resolve( 2 + 2 ) ).to.eventually.equal( 4 );
-        // expect( Promise.resolve( 2 + 2 ) ).to.eventually.equal( 4 ).and.notify( done );
-        expect( Promise.resolve( 4 ) ).to.eventually.equal( 4 ).and.notify( done );
+    context( '(working with "Chai as Promised")', function() {
+        it( 'passes one canary test', function() {
+            return expect( Promise.resolve( 2 + 2 ) ).to.eventually.equal( 4 );
+        } );
+
+        it( 'passes another canary test', function( done ) {
+            expect( Promise.resolve( 2 + 2 ) ).to.eventually.equal( 4 ).and.notify( done );
+        } );
+    } );
+
+    context( 'has a function `initialize`, which', function() {
+        // TODO >>> Add tests <<<
+    } );
+
+    context( 'has a function `storeToken`, which', function() {
+        // TODO >>> Add tests <<<
+    } );
+
+    context( 'has a function `verifyToken`, which', function() {
+        it( 'throws an error if the `token` argument is not a string' );
+
+        context( '(if the token is in the store)', function() {
+            it( 'returns true' );
+
+            it( 'removes the token from the store' );
+        } );
+
+        it( '(if the token is not the store) returns false', function() {} );
     } );
 } );
